6a1353646fe99b9f39e9de373efd8dddc14ec328
Moriel Schottlender <moriel@gmail.com>
2021-06-09 22:06:25 -0700

    Add marquee effect for 2000

--- components/MainTitle.vue
+++ components/MainTitle.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row
-      v-if="$store.getters.getCurrentSiteStyle === '2021'"
+      v-if="isCurrent(2021)"
       justify="center"
       align="center"
     >
@@ -11,25 +11,26 @@
         </h1>
       </v-col>
     </v-row>
-    <v-row
-      v-else
-    >
-      <v-col>
-        <!-- eslint-disable-next-line vue/html-self-closing -->
-        <hr
-          v-if="$store.getters.getCurrentSiteStyle !== '2021'"
-        />
-        <h1
-          :class="$store.getters.getCurrentSiteStyle >= 2000 ? 'text-center' : ''"
-        >
+    <div v-else-if="isCurrent(2000)">
+      <!-- eslint-disable-next-line vue/html-self-closing -->
+      <hr />
+      <div class="marquee">
+        <h1>
           Hi, I'm Moriel, {{ introSentence }}
         </h1>
-        <!-- eslint-disable-next-line vue/html-self-closing -->
-        <hr
-          v-if="$store.getters.getCurrentSiteStyle !== '2021'"
-        />
-      </v-col>
-    </v-row>
+      </div>
+      <!-- eslint-disable-next-line vue/html-self-closing -->
+      <hr />
+    </div>
+    <div v-else-if="isCurrent(1995)">
+      <!-- eslint-disable-next-line vue/html-self-closing -->
+      <hr />
+      <h1 class="my-2">
+        Hi, I'm Moriel, {{ introSentence }}
+      </h1>
+      <!-- eslint-disable-next-line vue/html-self-closing -->
+      <hr class="mb-5" />
+    </div>
   </div>
 </template>
 
@@ -40,7 +41,8 @@ export default {
   name: 'MainTitle',
   computed: {
     ...mapGetters([
-      'getWhatIDo'
+      'getWhatIDo',
+      'getCurrentSiteStyle'
     ]),
     introSentence () {
       switch (this.getWhatIDo) {
@@ -53,6 +55,69 @@ export default {
           return 'and I build stuff'
       }
     }
+  },
+  methods: {
+    isCurrent (year) {
+      return Number(this.getCurrentSiteStyle) === year
+    },
+    isCurrentHigherThan (year) {
+      return Number(this.getCurrentSiteStyle) > year
+    },
+    isCurrentLowerThan (year) {
+      return Number(this.getCurrentSiteStyle) < year
+    }
   }
 }
 </script>
+
+<style>
+/* https://www.quackit.com/html/html_editors/scratchpad/?example=/css/codes/marquees/scrolling_text */
+.marquee {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+}
+
+.marquee h1 {
+  font-size: 3em;
+  color: limegreen;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  line-height: 50px;
+  text-align: center;
+  -moz-transform: translateX(100%);
+  -webkit-transform: translateX(100%);
+  transform: translateX(100%);
+  -moz-animation: marquee 15s linear infinite;
+  -webkit-animation: marquee 15s linear infinite;
+  animation: marquee 15s linear infinite;
+}
+
+@-moz-keyframes marquee {
+  0% {
+    -moz-transform: translateX(100%);
+  }
+  100% { -moz-transform: translateX(-100%); }
+}
+
+@-webkit-keyframes marquee {
+  0% { -webkit-transform: translateX(100%); }
+  100% { -webkit-transform: translateX(-100%); }
+}
+
+@keyframes marquee {
+  0% {
+    -moz-transform: translateX(100%);
+    -webkit-transform: translateX(100%);
+    transform: translateX(100%);
+  }
+
+  100% {
+    -moz-transform: translateX(-100%);
+    -webkit-transform: translateX(-100%);
+    transform: translateX(-100%);
+  }
+}
+</style>
